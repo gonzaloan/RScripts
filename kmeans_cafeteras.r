9ce3601498bb25f465c331ad8b95dcf7deed0ea8
Gonzalo Muñoz <gonzalo.munoz.ext@sodexo.com>
2019-07-24 09:34:47 -0400

    se crea endpoint que retorna clusters

--- kmeans_cafeteras.r
+++ kmeans_cafeteras.r
@@ -1,10 +1,11 @@
 
-cafeteras<- read.csv("D:\\Gmunoz\\Desarrollo\\Propios\\R\\RScripts\\cafeteras_clean_data.csv")
+cafeteras<- read.csv("C:/gmunoz/propios/r/RScripts/cafeteras_clean_data.csv")
 
 #Eliminamos fila de nombre, que no nos servira mucho
 cafeteras<-cafeteras[,-1]
 cafeteras<-cafeteras[,-1]
 cafeteras<-cafeteras[,-4]
+
 
 #Se debe escalar los datos, para que no haya mucha desigualdad. Resta la media a los datos y los divide entre la varianza.
 cafeteras<-scale(cafeteras)
@@ -14,27 +15,28 @@ cafeteras<-scale(cafeteras)
 mycluster<-kmeans(cafeteras,3, nstart=5, iter.max = 30)
 
 #Elresultado son 3 clusters de 110, 100 y30.
-#Dice cuáles son los centros de cada cluster y campo
-#Muestra el conjunto de datos, en qué cluster está cada elemento.
+#Dice cu?les son los centros de cada cluster y campo
+#Muestra el conjunto de datos, en qu? cluster est? cada elemento.
 #Muestra suma de las distancias
 
 
 #Algunas funciones interesantes
 
-#Muestra número de iteraciones realizadas
+#Muestra n?mero de iteraciones realizadas
 mycluster$iter 
 
-#tamaño de los clusters
+#tama?o de los clusters
 mycluster$size
 
 #Muestra las medias de cada columna y cluster
 mycluster$centers
 
 #Distancias de error
-#Esto muestra qué tan buena es nuestra clusterizacion, mienrtas más pequeños los valores, más cercanos están de los centros.
+#Esto muestra qu? tan buena es nuestra clusterizacion, mienrtas m?s peque?os los valores, m?s cercanos est?n de los centros.
 mycluster$withinss
 
 
+mycluster
 #Analizar el withinss
 
 #Todas las distancias a sus centros para cada cluster.
@@ -54,94 +56,94 @@ mycluster<- kmeans(cafeteras, 8, nstart=5, iter.max = 30)
 
 library(fmsb)
 
-#partir la visualización en dos filas, y en cada fila 4 gráficas
+#partir la visualizaci?n en dos filas, y en cada fila 4 gr?ficas
 par(mfrow=c(2,4))
-#vamos a ir sacando cada una de las gráficas, para ello, necesitamos crear un subconjunto para cada uno, tiene que tener
-#Valor minimo, valor máximo, y valor de los centros para cada una de las variables
+#vamos a ir sacando cada una de las gr?ficas, para ello, necesitamos crear un subconjunto para cada uno, tiene que tener
+#Valor minimo, valor m?ximo, y valor de los centros para cada una de las variables
 dat<-as.data.frame(t(mycluster$centers[1,]))
 dat
-#Ahora agregamos a todos valores mínimos y máximos
+#Ahora agregamos a todos valores m?nimos y m?ximos
 dat<-rbind(rep(5,10), rep(-1,5,10), dat)
-#Esto nos da como resultado tres linas, la primera con valores máximos 5
-#La segunda con los valores mínimos -1
+#Esto nos da como resultado tres linas, la primera con valores m?ximos 5
+#La segunda con los valores m?nimos -1
 #La tercera con los valores que tenemos
 dat
 
-#Vemos el gráfico
+#Vemos el gr?fico
 radarchart(dat)
 
 #Hacemos lo mismo para los otros 
 
 dat<-as.data.frame(t(mycluster$centers[2,]))
 dat
-#Ahora agregamos a todos valores mínimos y máximos
+#Ahora agregamos a todos valores m?nimos y m?ximos
 dat<-rbind(rep(5,10), rep(-1,5,10), dat)
-#Esto nos da como resultado tres linas, la primera con valores máximos 5
-#La segunda con los valores mínimos -1
+#Esto nos da como resultado tres linas, la primera con valores m?ximos 5
+#La segunda con los valores m?nimos -1
 #La tercera con los valores que tenemos
 dat
 
-#Vemos el gráfico
+#Vemos el gr?fico
 radarchart(dat)
 
 dat<-as.data.frame(t(mycluster$centers[3,]))
 dat
-#Ahora agregamos a todos valores mínimos y máximos
+#Ahora agregamos a todos valores m?nimos y m?ximos
 dat<-rbind(rep(5,10), rep(-1,5,10), dat)
-#Esto nos da como resultado tres linas, la primera con valores máximos 5
-#La segunda con los valores mínimos -1
+#Esto nos da como resultado tres linas, la primera con valores m?ximos 5
+#La segunda con los valores m?nimos -1
 #La tercera con los valores que tenemos
 dat
 
-#Vemos el gráfico
+#Vemos el gr?fico
 radarchart(dat)
 
 dat<-as.data.frame(t(mycluster$centers[4,]))
 dat
-#Ahora agregamos a todos valores mínimos y máximos
+#Ahora agregamos a todos valores m?nimos y m?ximos
 dat<-rbind(rep(5,10), rep(-1,5,10), dat)
-#Esto nos da como resultado tres linas, la primera con valores máximos 5
-#La segunda con los valores mínimos -1
+#Esto nos da como resultado tres linas, la primera con valores m?ximos 5
+#La segunda con los valores m?nimos -1
 #La tercera con los valores que tenemos
 dat
 
-#Vemos el gráfico
+#Vemos el gr?fico
 radarchart(dat)
 
 dat<-as.data.frame(t(mycluster$centers[5,]))
 dat
-#Ahora agregamos a todos valores mínimos y máximos
+#Ahora agregamos a todos valores m?nimos y m?ximos
 dat<-rbind(rep(5,10), rep(-1,5,10), dat)
-#Esto nos da como resultado tres linas, la primera con valores máximos 5
-#La segunda con los valores mínimos -1
+#Esto nos da como resultado tres linas, la primera con valores m?ximos 5
+#La segunda con los valores m?nimos -1
 #La tercera con los valores que tenemos
 dat
 
-#Vemos el gráfico
+#Vemos el gr?fico
 radarchart(dat)
 
 dat<-as.data.frame(t(mycluster$centers[6,]))
 dat
-#Ahora agregamos a todos valores mínimos y máximos
+#Ahora agregamos a todos valores m?nimos y m?ximos
 dat<-rbind(rep(5,10), rep(-1,5,10), dat)
-#Esto nos da como resultado tres linas, la primera con valores máximos 5
-#La segunda con los valores mínimos -1
+#Esto nos da como resultado tres linas, la primera con valores m?ximos 5
+#La segunda con los valores m?nimos -1
 #La tercera con los valores que tenemos
 dat
 
-#Vemos el gráfico
+#Vemos el gr?fico
 radarchart(dat)
 
 dat<-as.data.frame(t(mycluster$centers[7,]))
 dat
-#Ahora agregamos a todos valores mínimos y máximos
+#Ahora agregamos a todos valores m?nimos y m?ximos
 dat<-rbind(rep(5,10), rep(-1,5,10), dat)
-#Esto nos da como resultado tres linas, la primera con valores máximos 5
-#La segunda con los valores mínimos -1
+#Esto nos da como resultado tres linas, la primera con valores m?ximos 5
+#La segunda con los valores m?nimos -1
 #La tercera con los valores que tenemos
 dat
 
-#Vemos el gráfico
+#Vemos el gr?fico
 radarchart(dat)
 
 
